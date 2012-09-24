385b442858670e0da29f50daa47fd136afd477ff
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2012-09-24 10:30:20 -0400

    Refactor and improve commits_spec
    Add:
     * Checks for nil files for spec_helpers#setup_commits
     * Spec for Commits#flush_commits
    
    Refactor:
     * commits_spec to use let and subject features of Rspec

--- spec/commits_spec.rb
+++ spec/commits_spec.rb
@@ -2,145 +2,190 @@ require File.dirname(__FILE__) + '/spec_helper'
 include GitStatistics
 
 describe Commits do
-  collector = Collector.new(false, 100, true)
-  commits = Commits.new(collector.repo_path, true)
+  let(:verbose) {false}
+  let(:limit) {100}
+  let(:fresh) {true}
+  let(:collector) {Collector.new(verbose, limit, fresh)}
+
+  let(:commits) {collector.commits}
+
+  let(:fixture_file) {"multiple_authors.json"}
+  let(:save_file) {collector.repo_path + "0.json"}
+  let(:pretty) {false}
+  let(:email) {false}
+  let(:merge) {false}
+  let(:sort_type) {:commits}
+  let(:stats) do
+    setup_commits(commits, fixture_file, save_file, pretty)
+    commits.calculate_statistics(email, merge)
+    commits.author_top_n_type(sort_type)
+  end
+
+  describe "#flush_commits" do
+    let(:commits) {collector.commits.load(fixture(fixture_file))}
+
+    context "with commits exceeding limit" do
+      let(:limit) {2}
+      it do
+        commits.size.should == 3
+        commits.flush_commits
+        commits.size.should == 0
+      end
+    end
+
+    context "with commits equal to limit" do
+      let(:limit) {3}
+      it do
+        commits.size.should == 3
+        commits.flush_commits
+        commits.size.should == 0
+      end
+    end
+
+    context "with commits less than limit" do
+      let(:limit) {5}
+      it do
+        commits.size.should == 3
+        commits.flush_commits
+        commits.size.should == 3
+      end
+    end
+
+    context "with commits less than limit but forced" do
+      let(:limit) {5}
+      it do
+        commits.size.should == 3
+        commits.flush_commits(true)
+        commits.size.should == 0
+      end
+    end
+  end
 
   describe "#author_top_n_type" do
+    let(:sort_type) {:deletions}
+
     context "with valid data" do
-      setup_commits(commits, "multiple_authors.json", collector.repo_path + "0.json", false)
+      context "on first author" do
+        author = "John Smith"
+        subject {stats[author]}
+        it {stats.has_key?(author).should be_true}
+        it {subject[:commits].should == 1}
+        it {subject[:deletions].should == 16}
+        it {subject[:additions].should == 64}
 
-      commits.calculate_statistics(false, false)
-      stats = commits.author_top_n_type(:deletions)
+        it {subject[:languages][:Ruby][:additions].should == 64}
+        it {subject[:languages][:Ruby][:deletions].should == 16}
+      end
 
-      # Check stats for first author
-      author_1 = "John Smith"
-      it {stats.has_key?(author_1).should be_true}
-      it {stats[author_1][:commits].should == 1}
-      it {stats[author_1][:additions].should == 64}
-      it {stats[author_1][:deletions].should == 16}
+      context "on second author" do
+        author = "Kevin Jalbert"
+        subject {stats[author]}
+        it {stats.has_key?(author).should be_true}
+        it {subject[:commits].should == 1}
+        it {subject[:additions].should == 73}
+        it {subject[:deletions].should == 0}
+        it {subject[:create].should == 2}
 
-      it {stats[author_1][:languages][:Ruby][:additions].should == 64}
-      it {stats[author_1][:languages][:Ruby][:deletions].should == 16}
-
-      # Check stats for second author
-      author_2 = "Kevin Jalbert"
-      it {stats.has_key?(author_2).should be_true}
-      it {stats[author_2][:commits].should == 1}
-      it {stats[author_2][:additions].should == 73}
-      it {stats[author_2][:deletions].should == 0}
-      it {stats[author_2][:create].should == 2}
-
-      it {stats[author_2][:languages][:Markdown][:additions].should == 11}
-      it {stats[author_2][:languages][:Markdown][:deletions].should == 0}
-      it {stats[author_2][:languages][:Markdown][:create].should== 1}
-      it {stats[author_2][:languages][:Ruby][:additions].should == 62}
-      it {stats[author_2][:languages][:Ruby][:deletions].should == 0}
-      it {stats[author_2][:languages][:Ruby][:create].should == 1}
-
-      commits.clean
+        it {subject[:languages][:Markdown][:additions].should == 11}
+        it {subject[:languages][:Markdown][:deletions].should == 0}
+        it {subject[:languages][:Markdown][:create].should== 1}
+        it {subject[:languages][:Ruby][:additions].should == 62}
+        it {subject[:languages][:Ruby][:deletions].should == 0}
+        it {subject[:languages][:Ruby][:create].should == 1}
+      end
     end
 
     context "with invalid type" do
-      setup_commits(commits, "multiple_authors.json", collector.repo_path + "0.json", false)
-
-      commits.calculate_statistics(false, false)
-      stats = commits.author_top_n_type(:wrong)
-
+      let(:sort_type) {:wrong}
       it {stats.should == nil}
-
-      commits.clean
     end
 
     context "with invalid data" do
-      commits.calculate_statistics(false, false)
-      stats = commits.author_top_n_type(:deletions)
-
+      let(:fixture_file) {nil}
       it {stats.should == nil}
-
-      commits.clean
     end
   end
 
   describe "#calculate_statistics" do
+    let(:fixture_file) {"single_author_pretty.json"}
+
     context "with email" do
-      setup_commits(commits, "single_author_pretty.json", collector.repo_path + "0.json", false)
-
-      commits.calculate_statistics(true, false)
-      stats = commits.author_top_n_type(:commits)
-
+      let(:email) {true}
       author = "kevin.j.jalbert@gmail.com"
+      subject {stats[author]}
+
       it {stats.has_key?(author).should be_true}
-      it {stats[author][:commits].should == 1}
-      it {stats[author][:additions].should == 73}
-      it {stats[author][:deletions].should == 0}
-      it {stats[author][:create].should == 2}
+      it {subject[:commits].should == 1}
+      it {subject[:additions].should == 73}
+      it {subject[:deletions].should == 0}
+      it {subject[:create].should == 2}
 
-      it {stats[author][:languages][:Markdown][:additions].should == 11}
-      it {stats[author][:languages][:Markdown][:deletions].should == 0}
-      it {stats[author][:languages][:Ruby][:additions].should == 62}
-      it {stats[author][:languages][:Ruby][:deletions].should == 0}
-      it {stats[author][:languages][:Ruby][:create].should == 1}
-
-      commits.clean
+      it {subject[:languages][:Markdown][:additions].should == 11}
+      it {subject[:languages][:Markdown][:deletions].should == 0}
+      it {subject[:languages][:Ruby][:additions].should == 62}
+      it {subject[:languages][:Ruby][:deletions].should == 0}
+      it {subject[:languages][:Ruby][:create].should == 1}
     end
 
     context "with merge" do
-      setup_commits(commits, "single_author_pretty.json", collector.repo_path + "0.json", false)
-
-      commits.calculate_statistics(false, true)
-      stats = commits.author_top_n_type(:commits)
-
+      let(:merge) {true}
       author = "Kevin Jalbert"
+      subject {stats[author]}
+
       it {stats.has_key?(author).should be_true}
-      it {stats[author][:commits].should == 2}
-      it {stats[author][:additions].should == 153}
-      it {stats[author][:deletions].should == 5}
-      it {stats[author][:create].should == 3}
-      it {stats[author][:merges].should == 1}
+      it {subject[:commits].should == 2}
+      it {subject[:additions].should == 153}
+      it {subject[:deletions].should == 5}
+      it {subject[:create].should == 3}
+      it {subject[:merges].should == 1}
 
-      it {stats[author][:languages][:Markdown][:additions].should == 18}
-      it {stats[author][:languages][:Markdown][:deletions].should == 1}
-      it {stats[author][:languages][:Ruby][:additions].should == 135}
-      it {stats[author][:languages][:Ruby][:deletions].should == 4}
-      it {stats[author][:languages][:Ruby][:create].should == 2}
-
-      commits.clean
+      it {subject[:languages][:Markdown][:additions].should == 18}
+      it {subject[:languages][:Markdown][:deletions].should == 1}
+      it {subject[:languages][:Ruby][:additions].should == 135}
+      it {subject[:languages][:Ruby][:deletions].should == 4}
+      it {subject[:languages][:Ruby][:create].should == 2}
     end
   end
 
   describe "#add_language_stats" do
-    file = {:additions => 10,
-            :deletions => 5}
 
     context "with file language" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
-      data[:languages] = {}
+      let(:data) {
+        data = Hash.new(0)
+        data[:languages] = {}
 
-      file[:language] = "Ruby"
+        file = {:additions => 10,
+                :deletions => 5}
 
-      data = commits.add_language_stats(data, file)
+        file[:language] = "Ruby"
+
+        data = commits.add_language_stats(data, file)
+      }
 
       it {data[:languages][:Ruby][:additions].should == 10}
       it {data[:languages][:Ruby][:deletions].should == 5}
     end
 
     context "with multiple files" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
-      data[:languages] = {}
+      let(:data) {
+        data = Hash.new(0)
+        data[:languages] = {}
 
-      # First file is "Ruby"
-      file[:language] = "Ruby"
-      data = commits.add_language_stats(data, file)
+        file = {:additions => 10,
+                :deletions => 5}
 
-      # Second file is "Java"
-      file[:language] = "Java"
-      data = commits.add_language_stats(data, file)
+        # First file is "Ruby"
+        file[:language] = "Ruby"
+        data = commits.add_language_stats(data, file)
 
-      # Third file is "Ruby"
-      file[:language] = "Ruby"
-      data = commits.add_language_stats(data, file)
+        # Second file is "Java"
+        file[:language] = "Java"
+        data = commits.add_language_stats(data, file)
+
+        # Third file is "Ruby"
+        file[:language] = "Ruby"
+        data = commits.add_language_stats(data, file)
+      }
 
       it {data[:languages][:Ruby][:additions].should == 20}
       it {data[:languages][:Ruby][:deletions].should == 10}
@@ -150,15 +195,15 @@ describe Commits do
   end
 
   describe "#add_commit_stats" do
-    commit = {:additions => 10,
-              :deletions => 5,
-              :merge => false}
-
     context "with valid commit" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
+      let(:data) {
+        commit = {:additions => 10,
+                  :deletions => 5,
+                  :merge => false}
 
-      data = commits.add_commit_stats(data, commit)
+        data = Hash.new(0)
+        data = commits.add_commit_stats(data, commit)
+      }
 
       it {data[:commits].should == 1}
       it {data[:additions].should == 10}
@@ -166,16 +211,19 @@ describe Commits do
       it {data[:merges].should == 0}
     end
 
-    context "with multiple commits" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
+    context "with multiple commits (one merge commit)" do
+      let(:data) {
+        commit = {:additions => 10,
+                  :deletions => 5,
+                  :merge => false}
 
-      data = commits.add_commit_stats(data, commit)
+        data = Hash.new(0)
+        data = commits.add_commit_stats(data, commit)
 
-      # Second commit has merge status
-      commit[:merge] = true
-
-      data = commits.add_commit_stats(data, commit)
+        # Second commit has merge status
+        commit[:merge] = true
+        data = commits.add_commit_stats(data, commit)
+      }
 
       it {data[:commits].should == 2}
       it {data[:additions].should == 20}
@@ -184,14 +232,18 @@ describe Commits do
     end
 
     context "with commit that has file status changes" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
-      commit[:create] = 1
-      commit[:delete] = 2
-      commit[:rename] = 3
-      commit[:copy] = 4
+      let(:data) {
+        commit = {:additions => 10,
+                  :deletions => 5,
+                  :create => 1,
+                  :delete => 2,
+                  :rename => 3,
+                  :copy => 4,
+                  :merge => false}
 
-      data = commits.add_commit_stats(data, commit)
+        data = Hash.new(0)
+        data = commits.add_commit_stats(data, commit)
+      }
 
       it {data[:commits].should == 1}
       it {data[:additions].should == 10}
@@ -201,42 +253,37 @@ describe Commits do
       it {data[:rename].should == 3}
       it {data[:copy].should == 4}
     end
-
-    context "with merge commit" do
-      commits = Commits.new(collector.repo_path, true)
-      data = Hash.new(0)
-      commit[:merge] = true
-
-      data = commits.add_commit_stats(data, commit)
-
-      it {data[:commits].should == 1}
-      it {data[:additions].should == 10}
-      it {data[:deletions].should == 5}
-      it {data[:merges].should == 1}
-    end
   end
 
   describe "#save and #load" do
     context "with pretty" do
-      commits = Commits.new(collector.repo_path, true)
-      commits.load(fixture("single_author_pretty.json"))
-      commits.save("tmp.json", true)
+      let(:fixture_file) {"single_author_pretty.json"}
+      let(:pretty) {true}
 
-      same = FileUtils.compare_file("tmp.json", fixture("single_author_pretty.json"))
-      FileUtils.remove_file("tmp.json")
+      it do
+        commits.load(fixture(fixture_file))
+        commits.save("tmp.json", pretty)
 
-      it {same.should be_true}
+        same = FileUtils.compare_file("tmp.json", fixture(fixture_file))
+        FileUtils.remove_file("tmp.json")
+
+        same.should be_true
+      end
     end
 
     context "with no pretty" do
-      commits = Commits.new(collector.repo_path, true)
-      commits.load(fixture("multiple_authors.json"))
-      commits.save("tmp.json", false)
+      let(:fixture_file) {"multiple_authors.json"}
+      let(:pretty) {false}
 
-      same = FileUtils.compare_file("tmp.json", fixture("multiple_authors.json"))
-      FileUtils.remove_file("tmp.json")
+      it do
+        commits.load(fixture(fixture_file))
+        commits.save("tmp.json", pretty)
 
-      it {same.should be_true}
-   end
+        same = FileUtils.compare_file("tmp.json", fixture(fixture_file))
+        FileUtils.remove_file("tmp.json")
+
+        same.should be_true
+      end
+    end
   end
 end
