1c910d03a85e924881eb1e31274a3b3c2558fa80
Matt Bridges <mbridges.91@gmail.com>
2013-01-13 17:22:10 -0600

    small syntax changes for clarity and better reading

--- lib/git_statistics/utilities.rb
+++ lib/git_statistics/utilities.rb
@@ -85,9 +85,9 @@ module GitStatistics
 
     def self.get_modified_time(file)
       command = case
-                when OS.mac? then "stat -f %m #{file}"
-                when OS.linux? then "stat -c %Y #{file}"
-                else raise "Update on the Windows operating system is not supported"; end
+        when OS.mac? then "stat -f %m #{file}"
+        when OS.linux? then "stat -c %Y #{file}"
+        else raise "Update on the Windows operating system is not supported"; end
       time_at(command)
     end
 
