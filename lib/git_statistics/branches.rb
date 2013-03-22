6a1aea6ff40ec898b09de6916f460fdd5fa74998
Matt Bridges <mbridges.91@gmail.com>
2013-03-21 21:22:24 -0500

    prefer `#strip` to `#clean_for_authors`

--- lib/git_statistics/branches.rb
+++ lib/git_statistics/branches.rb
@@ -2,7 +2,7 @@ module GitStatistics
   class Branches
     def self.collect
       pipe.collect do |branch|
-        branch.clean_for_authors.sub(/\A\*\s/, "")
+        branch.strip.sub(/\A\*\s/, "")
       end.reject { |b| b == '(no branch)' }
     end
 
