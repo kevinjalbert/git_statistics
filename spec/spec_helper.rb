e2a77ddd42647d5fdc07e86b3c6aa78dabc3167c
Matt Bridges <mbridges.91@gmail.com>
2013-03-29 10:30:25 -0500

    Stub logger output in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,11 @@ def setup_commits(commits, file_load, file_save, pretty)
   commits.load(fixture(file_load))
   commits.save(file_save, pretty)
 end
+
+RSpec.configure do |config|
+  config.before do
+    %w[debug info warn error fatal].each do |level|
+      GitStatistics::Log.stub(level)
+    end
+  end
+end
