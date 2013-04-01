17b64522a6ce08749dadc865e512b072e624fd0e
Matt Bridges <mbridges.91@gmail.com>
2013-04-01 10:30:05 -0500

    Small refactoring to simplify

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -5,17 +5,19 @@ describe Repository do
   describe "#find" do
     subject { Repository.find(dir) }
 
+    let(:current) { Pathname.new(Dir.pwd) }
+
     before do
       Thread.current[:repository] = nil
     end
 
     context "with root directory" do
-      let(:dir) { Dir.pwd } # git_statistics/
+      let(:dir) { current }
       it { should be_a Grit::Repo }
     end
 
     context "with sub directory" do
-      let(:dir) { File.dirname(__FILE__) } # git_statistics/spec/
+      let(:dir) { current + "spec" } # git_statistics/spec/
       it { should be_a Grit::Repo }
     end
 
