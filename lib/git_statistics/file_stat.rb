3910eff5eb78ba443268f57fd8de190f262ae079
Matt Bridges <mbridges.91@gmail.com>
2013-04-03 17:01:38 -0500

    Make FileStat more blob and submodule aware

--- lib/git_statistics/file_stat.rb
+++ lib/git_statistics/file_stat.rb
@@ -10,13 +10,37 @@ module GitStatistics
       @tree / filename
     end
 
+    def binary?
+      submodule? ? false : blob.binary?
+    end
+
+    def image?
+      submodule? ? false : blob.binary?
+    end
+
+    def vendored?
+      submodule? ? false : blob.vendored?
+    end
+
+    def generated?
+      submodule? ? false : blob.generated?
+    end
+
+    def submodule?
+      blob.kind_of? Grit::Submodule
+    end
+
     def inspect
       %Q{<GitStatistics::FileStat @language=#{language} @additions=#{additions}, @deletions=#{deletions}, @net=#{net}>}
     end
 
     # Determine the language of the file from the blob
     def language
-      (blob && blob.language && blob.language.name) || "Unknown"
+      if submodule?
+        "Unknown"
+      else
+        (blob && blob.language && blob.language.name) || "Unknown"
+      end
     end
   end
 end
