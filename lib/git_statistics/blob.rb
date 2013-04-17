83e3437837356ac4c61b29003d05de57579ef899
Matt Bridges <mbridges.91@gmail.com>
2013-04-17 15:43:26 -0500

    Moving LanguageSniffer::BlobHelper includion into initializer

--- lib/git_statistics/blob.rb
+++
@@ -1,6 +0,0 @@
-require 'language_sniffer'
-module Grit
-  class Blob
-    include LanguageSniffer::BlobHelper
-  end
-end
--- lib/git_statistics/initialize.rb
+++ lib/git_statistics/initialize.rb
@@ -1,11 +1,18 @@
 require 'json'
-require 'grit'
 require 'pathname'
 require 'ostruct'
 require 'optparse'
 
+require 'grit'
+require 'language_sniffer'
+
+module Grit
+  class Blob
+    include ::LanguageSniffer::BlobHelper
+  end
+end
+
 # Must be required before all other files
-require 'git_statistics/blob'
 require 'git_statistics/regex_matcher'
 
 Dir.glob(File.dirname(__FILE__) + '/**/*.rb') {|file| require file}
