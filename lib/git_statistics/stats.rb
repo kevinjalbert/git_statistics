b7f97272c38b9b807a6544452509b7831434a668
Matt Bridges <mbridges.91@gmail.com>
2013-04-29 17:39:38 -0700

    Improve Stats#take_top implementation and add specs

--- lib/git_statistics/stats.rb
+++ lib/git_statistics/stats.rb
@@ -1,6 +1,11 @@
 module GitStatistics
   class Stats < Hash
 
+    def initialize(values = {})
+      super()
+      values.each { |k,v| self[k] = v}
+    end
+
     def sorted(&block)
       self.class[sort(&block)]
     end
@@ -10,8 +15,8 @@ module GitStatistics
     end
 
     def take_top(top_n = 0)
-      top_n = 0 if top_n < 0
-      self.class[*to_a[0..(top_n - 1)].flatten]
+      top_n = self.size if top_n.nil? || top_n <= 0
+      self.class[to_a.take(top_n)]
     end
 
   end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 include GitStatistics
 
 describe Stats do
+  let(:values) { {} }
+  subject(:stats)  { Stats.new(values) }
+
+  context "#take_top" do
+    let(:values) { { 1=>2, 3=>4, 5=>6, 7=>8 } }
+    subject { stats.take_top(number) }
+
+    context "taking nil should return all" do
+      let(:number) { nil }
+      it { should == stats }
+    end
+
+    context "taking 0 should return all" do
+      let(:number) { 0 }
+      it { should == stats }
+    end
+
+    context "taking 1 should return first key=>value pair" do
+      let(:number) { 1 }
+      it { should == Stats.new(1=>2) }
+    end
+
+    context "taking 5 should return all values" do
+      let(:number) { 5 }
+      it { should == stats }
+    end
+  end
+
 end
