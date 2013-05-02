c93304344499cce7db987e8c9e1345e2a98fa35c
Matt Bridges <mbridges.91@gmail.com>
2013-05-02 09:45:22 -0700

    Use splat groups for clarity
    This clarifies the values in the array that is passed to the block

--- lib/git_statistics/commit_line_extractor.rb
+++ lib/git_statistics/commit_line_extractor.rb
@@ -15,36 +15,35 @@ module GitStatistics
     end
 
     def changed
-      modified_or_renamed = ModifiedOrRenamed.if_matches(line) do |changes|
-        split_file = Utilities.split_old_new_file(changes[2], changes[3])
-        {:additions => changes[0].to_i,
-          :deletions => changes[1].to_i,
+      modified_or_renamed = ModifiedOrRenamed.if_matches(line) do |(additions, deletions, old_file, new_file)|
+        split_file = Utilities.split_old_new_file(old_file, new_file)
+        {:additions => additions.to_i,
+          :deletions => deletions.to_i,
           :file => split_file[:new_file],
           :old_file => split_file[:old_file]}
       end
       return modified_or_renamed unless modified_or_renamed.empty?
 
-      AdditionsOrDeletions.if_matches(line) do |changes|
-        {:additions => changes[0].to_i,
-          :deletions => changes[1].to_i,
-          :file => changes[2]}
+      AdditionsOrDeletions.if_matches(line) do |(additions, deletions, file)|
+        {:additions => additions.to_i,
+          :deletions => deletions.to_i,
+          :file => file}
       end
     end
 
     def created_or_deleted
-      CreatedOrDeleted.if_matches(line) do |changes|
-        {:status => changes[0],
-          :file => changes[1]}
+      CreatedOrDeleted.if_matches(line) do |(status, file)|
+        {:status => status, :file => file}
       end
     end
 
     def renamed_or_copied
-      RenamedOrCopied.if_matches(line) do |changes|
-        split_file = Utilities.split_old_new_file(changes[1], changes[2])
-        {:status => changes[0],
+      RenamedOrCopied.if_matches(line) do |(status, old_file, new_file, similar)|
+        split_file = Utilities.split_old_new_file(old_file, new_file)
+        {:status => status,
           :old_file => split_file[:old_file],
           :new_file => split_file[:new_file],
-          :similar => changes[3].to_i}
+          :similar => similar.to_i}
       end
     end
 
